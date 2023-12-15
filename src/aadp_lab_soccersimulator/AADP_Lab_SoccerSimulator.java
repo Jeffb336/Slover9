74701d1189bbd6f7fb2b0ca6e93a6f7e3b4734e6
Yuna <yjang7795@gmail.com>
2023-12-15 13:46:36 +0000

    commit2
    commit 2

--- src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
+++ src/aadp_lab_soccersimulator/AADP_Lab_SoccerSimulator.java
@@ -16,17 +16,17 @@ import java.util.Scanner;
  * @author Sam
  * Please put the names and student numbers of the team here:
  * 
- * Name 1: 
- * Number 1:
+ * Name 1: Mirae Yu
+ * Number 1: 2021332
  * 
- * Name 2:
- * Number 2:
+ * Name 2: Jefferson Sousa Silva
+ * Number 2: 2021325
  * 
- * Name 3:
- * Number 3:
+ * Name 3: Yuna Jang
+ * Number 3: 2021297
  * 
- * Name 4:
- * Number 4
+ * Name 4: Tumurtulga batjargal
+ * Number 4: 2021284
  * 
  */
 
